09a02879e1f2483a8ac40dd0a7caa50487427ff6
prooooff <terk0381@gmail.com>
2026-03-22 22:43:33 +0200

    Fix DatabaseSeeder and add temporary user for Trip creation

---
+++ TravelManager/TravelManager.Infrastructure/Migrations/20260322204102_SeedFixFinal.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedFixFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "temporary-user-id",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "static-concurrency-stamp", "static-security-stamp" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "temporary-user-id",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "0426ed37-d3ba-40d3-8d3d-c0e303f3fa93", "50efc77a-6672-4c15-80d2-c26aaf26ab02" });
+        }
+    }
+}
--- TravelManager/TravelManager.UI/Controllers/TripsController.cs
+++ TravelManager/TravelManager.UI/Controllers/TripsController.cs
@@ -1,12 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TravelManager.Domain.Entities;
+using TravelManager.Infrastructure.Interfaces;
+using TravelManager.UI.Models.ViewModels;
 
 namespace TravelManager.UI.Controllers
 {
     public class TripsController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TripsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var tripsFromDb = _unitOfWork.Trip.GetAll();
+
+            var tripViewModels = tripsFromDb.Select(t => new TripListViewModel
+            {
+                Id = t.Id,
+                Title = t.Title,
+                DepartureLocation = t.DepartureLocation,
+                StartDate = t.StartDate,
+                EndDate = t.EndDate,
+                StatusName = "Заплановано"
+            }).ToList();
+
+            return View(tripViewModels);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreateTripViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateTripViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var newTrip = new Trip
+            {
+                Title = model.Title,
+                Description = model.Description,
+                DepartureLocation = model.DepartureLocation,
+                ReturnLocation = model.ReturnLocation,
+                StartDate = model.StartDate,
+                EndDate = model.EndDate,
+                BaseCurrency = model.BaseCurrency,
+                StatusId = 1,
+                CreatedAt = DateTime.UtcNow,
+                CreatorId = "temporary-user-id"
+            };
+
+            _unitOfWork.Trip.Add(newTrip);
+            await _unitOfWork.SaveAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
