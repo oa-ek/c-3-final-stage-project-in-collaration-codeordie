7d65d471f423b4a97790b4c22c9c9d74deea4a7f
prooooff <terk0381@gmail.com>
2026-03-22 22:17:32 +0200

    Add ViewModels for Trip entity

---
+++ TravelManager/TravelManager.UI/Models/ViewModels/CreateTripViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelManager.UI.Models.ViewModels
+{
+    public class CreateTripViewModel
+    {
+        [Required(ErrorMessage = "Введіть назву поїздки")]
+        [MaxLength(200, ErrorMessage = "Назва занадто довга")]
+        [Display(Name = "Назва поїздки")]
+        public string Title { get; set; }
+
+        [Display(Name = "Опис")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Вкажіть місце відправлення")]
+        [Display(Name = "Місце відправлення")]
+        public string DepartureLocation { get; set; }
+
+        [Display(Name = "Місце повернення")]
+        public string? ReturnLocation { get; set; }
+
+        [Required(ErrorMessage = "Вкажіть дату початку")]
+        [Display(Name = "Дата початку")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; } = DateTime.Today;
+
+        [Required(ErrorMessage = "Вкажіть дату завершення")]
+        [Display(Name = "Дата завершення")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; } = DateTime.Today.AddDays(7);
+
+        [Required(ErrorMessage = "Оберіть базову валюту (напр. USD, UAH)")]
+        [Display(Name = "Базова валюта")]
+        public string BaseCurrency { get; set; } = "UAH";
+    }
+}
---
+++ TravelManager/TravelManager.UI/Models/ViewModels/TripListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TravelManager.UI.Models.ViewModels
+{
+    public class TripListViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string DepartureLocation { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string StatusName { get; set; }
+    }
+}
