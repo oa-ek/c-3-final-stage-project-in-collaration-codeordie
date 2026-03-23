5c951a36f641ae4fa9fbe3c9a7c3e98293792d34
prooooff <terk0381@gmail.com>
2026-03-23 10:08:46 +0200

    Add complete CRUD for Accommodations with Trip relation

---
+++ TravelManager/TravelManager.UI/Models/ViewModels/AccommodationFormViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TravelManager.UI.Models.ViewModels
+{
+    public class AccommodationFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public DateTime CheckInTime { get; set; } = DateTime.Today.AddHours(14);
+
+        [Required]
+        public DateTime CheckOutTime { get; set; } = DateTime.Today.AddDays(1).AddHours(12);
+
+        public string? BookingReference { get; set; }
+
+        [Required(ErrorMessage = "Оберіть поїздку")]
+        public int TripId { get; set; }
+
+        public IEnumerable<SelectListItem>? TripList { get; set; }
+    }
+}
---
+++ TravelManager/TravelManager.UI/Models/ViewModels/AccommodationListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TravelManager.UI.Models.ViewModels
+{
+    public class AccommodationListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+    }
+}
